7edb81e01e9fa5676cdd0bad71fdfe8ab18fe08c
Tahseen Javed <iTahseenJaved@gmail.com>
2020-08-06 00:04:03 +0500

    Updated order id
    Added order id for bills

--- Server/serializers.py
+++ Server/serializers.py
@@ -22,7 +22,7 @@ class RepairOrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RepairOrderModel
 
-        fields = ['user_phone_number', 'mobile_brand', 'mobile_model', 'mobile_fault', 'image', 'date_order_placed',
+        fields = ['id', 'user_phone_number', 'mobile_brand', 'mobile_model', 'mobile_fault', 'image', 'date_order_placed',
                   'date_item_received', 'date_item_delivered', 'order_status', 'has_repaired', 'charges']
 
 
